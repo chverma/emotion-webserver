c4f46fc392bf71e67ee622a91df86138fe6685e4
chverma <chverma@upv.es>
2016-04-29 02:11:47 +0200

    Added compatibility with pythonLendmarkEmotionDetection

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# Node.Js-Express-And-PHPMyAdmin
-Sample app using node.js express using PHPMyAdmin database
+# Nodejs - Python - CameraWeb
+This is the web server of lendmark emotion detection on Nao robots. 
+This server will host an official website and some mini apps in order to get images to create a big database. 
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,10 @@ var router = express.Router();
 var util = require("util"); 
 var fs = require("fs");
 var path = require('path');
+var PythonShell = require('python-shell');
+
+
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -16,8 +20,9 @@ router.get('/captureImage', function(req, res, next) {
 router.post('/captureImage', function(req, res, next) {
 	//console.log("FormData "+ req.body.base64);
 
-	console.log("Image received from: "+ip);
-	var ip = req.body.ip
+	
+	//var ip = req.body.ip
+	//console.log("Image received captureImage from: "+ip);
 	var emotion = req.body.emotion;
 	var base64Data = req.body.base64.replace(/^data:image\/png;base64,/, "");
     var timestamp = new Date().getTime();
@@ -32,5 +37,45 @@ router.post('/captureImage', function(req, res, next) {
 	});
 });
 
+router.get('/svm', function(req, res, next) {
+		//res.render('index', { title: 'Express'});
+		res.render('svm', { title: 'SVM' });
+});
+
+router.post('/svm', function(req, res, next) {
+	//console.log("FormData "+ req.body.base64);
+	var emotion = req.body.emotion;
+    var base64Data = req.body.base64.replace(/^data:image\/png;base64,/, "");
+    var timestamp = new Date().getTime();
+    var filename  = emotion+"/web"+ timestamp +".png";
+	fs.writeFile("uploads/"+filename, base64Data, 'base64', function(err) {
+		if(err){
+		    
+			console.log("error writing file: "+err);
+		}else{
+		    var options = {
+                mode: 'text',
+                pythonPath: '/usr/bin/python',
+                pythonOptions: ['-u'],
+                scriptPath: '/home/chverma/UPV/TFG/pythonDlibLendmark',
+                args: [filename]
+            };
+            PythonShell.run('predictOneImage.py', options, function (err, results) {
+                if (err){
+                    console.log("predictOneImage-> Error")
+                    throw err;
+                }
+                // results is an array consisting of messages collected during execution
+                console.log('results: %j', results[0]);
+                var emotionMsg = "La teua emoció és: "+results.toString();
+                console.log(emotionMsg)
+                res.send(JSON.stringify({'status': 1, 'msg': emotionMsg.toString() }));
+            });
+			
+		}
+	});
+	
+
+});
 
 module.exports = router;
