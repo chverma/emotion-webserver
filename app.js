1f632fa98f46a7caf07b90cf1b24f2b780859c9f
chverma <chverma@upv.es>
2017-05-10 19:09:33 +0200

    Standarize code

--- app.js
+++ app.js
@@ -1,86 +1,83 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var express = require('express')
+var path = require('path')
+var logger = require('morgan')
+var cookieParser = require('cookie-parser')
+var bodyParser = require('body-parser')
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var routes = require('./routes/index')
+var users = require('./routes/users')
 
-// DataBase 
-var mysql = require("mysql");
+//  DataBase
+var mysql = require('mysql')
 var con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "nodeDb"
-});
-con.connect(function(err){
-  if(err){
-    console.log('Error connecting to Db');
-    return;
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'nodeDb'
+})
+con.connect(function (err) {
+  if (err) {
+    console.log('Error connecting to Db')
+    return
   }
-  console.log('Connection established');
-});
+  console.log('Connection established')
+})
 
-var app = express();
+var app = express()
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+//  view engine setup
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'jade')
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-//app.use(bodyParser.json());
-//app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json({limit: '50mb'}));
-app.use(bodyParser.raw({limit: '50mb'}));
-app.use(bodyParser.text({limit: '50mb'}));
-app.use(bodyParser.urlencoded({limit: '50mb', extended: false}));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-// Make our db accessible to our router
-app.use(function(req,res,next){
-    req.con = con;
-    next();
-});
+//  uncomment after placing your favicon in /public
+// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
+app.use(logger('dev'))
+// app.use(bodyParser.json())
+// app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json({limit: '50mb'}))
+app.use(bodyParser.raw({limit: '50mb'}))
+app.use(bodyParser.text({limit: '50mb'}))
+app.use(bodyParser.urlencoded({limit: '50mb', extended: false}))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
+//  Make our db accessible to our router
+app.use(function (req, res, next) {
+  req.con = con
+  next()
+})
 
-app.use('/', routes);
-app.use('/users', users);
+app.use('/', routes)
+app.use('/users', users)
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+//  catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  var err = new Error('Not Found')
+  err.status = 404
+  next(err)
+})
 
-// error handlers
+//  error handlers
 
-// development error handler
-// will print stacktrace
+//  development error handler
+//  will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
+  app.use(function (err, req, res, next) {
+    res.status(err.status || 500)
     res.render('error', {
       message: err.message,
       error: err
-    });
-  });
+    })
+  })
 }
 
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
+//  production error handler
+//  no stacktraces leaked to user
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500)
   res.render('error', {
     message: err.message,
     error: {}
-  });
-});
+  })
+})
 
-
-
-module.exports = app;
+module.exports = app
--- routes/index.js
+++ routes/index.js
@@ -1,81 +1,75 @@
-var express = require('express');
-var router = express.Router();
-var util = require("util"); 
-var fs = require("fs");
-var path = require('path');
-var PythonShell = require('python-shell');
-
-
-
+var express = require('express')
+var router = express.Router()
+var fs = require('fs')
+var PythonShell = require('python-shell')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-		//res.render('index', { title: 'Express'});
-		res.redirect("/captureImage");
-});
-router.get('/captureImage', function(req, res, next) {
-	res.render('captureImage', { title: 'Capture Image and upload' });
-});
+router.get('/', function (req, res, next) {
+  // res.render('index', { title: 'Express'});
+  res.redirect('/captureImage')
+})
 
-router.post('/captureImage', function(req, res, next) {
-	//console.log("FormData "+ req.body.base64);
+router.get('/captureImage', function (req, res, next) {
+  res.render('captureImage', { title: 'Capture Image and upload' })
+})
 
-	
-	//var ip = req.body.ip
-	//console.log("Image received captureImage from: "+ip);
-	var emotion = req.body.emotion;
-	var base64Data = req.body.base64.replace(/^data:image\/png;base64,/, "");
-    var timestamp = new Date().getTime();
-    
-	fs.writeFile("uploads/"+emotion+"/web"+ timestamp +".png", base64Data, 'base64', function(err) {
-		if(err){
-		    
-			console.log("error writing file: "+err);
-		}else{
-			res.send(JSON.stringify({'status': 1, 'msg': 'Image Uploaded'}));
-		}
-	});
-});
+router.post('/captureImage', function (req, res, next) {
+  // console.log("FormData "+ req.body.base64);
+  // var ip = req.body.ip
+  // console.log("Image received captureImage from: "+ip);
+  var emotion = req.body.emotion
+  var base64Data = req.body.base64.replace(/^data:image\/png;base64,/, '')
+  var timestamp = new Date().getTime()
 
-router.get('/svm', function(req, res, next) {
-		//res.render('index', { title: 'Express'});
-		res.render('svm', { title: 'SVM' });
-});
+  fs.writeFile('uploads/' + emotion + '/web' + timestamp + '.png', base64Data, 'base64', function (err) {
+    if (err) {
+      console.log('error writing file: ' + err)
+    } else {
+      res.send(JSON.stringify({'status': 1, 'msg': 'Image Uploaded'}))
+    }
+  })
+})
 
-router.post('/svm', function(req, res, next) {
-	//console.log("FormData "+ req.body.base64);
-	var emotion = req.body.emotion;
-    var base64Data = req.body.base64.replace(/^data:image\/png;base64,/, "");
-    var timestamp = new Date().getTime();
-    var filename  = emotion+"/web"+ timestamp +".png";
-	fs.writeFile("uploads/"+filename, base64Data, 'base64', function(err) {
-		if(err){
-		    
-			console.log("error writing file: "+err);
-		}else{
-		    var options = {
-                mode: 'text',
-                pythonPath: '/usr/bin/python',
-                pythonOptions: ['-u'],
-                scriptPath: '/home/chverma/UPV/TFG/pythonDlibLendmark',
-                args: [filename]
-            };
-            PythonShell.run('predictOneImage.py', options, function (err, results) {
-                if (err){
-                    console.log("predictOneImage-> Error")
-                    throw err;
-                }
-                // results is an array consisting of messages collected during execution
-                console.log('results: %j', results[0]);
-                var emotionMsg = "La teua emoció és: "+results.toString();
-                console.log(emotionMsg)
-                res.send(JSON.stringify({'status': 1, 'msg': emotionMsg.toString() }));
-            });
-			
-		}
-	});
-	
+router.get('/svm', function (req, res, next) {
+  // res.render('index', { title: 'Express'});
+  res.render('svm', {title: 'SVM'})
+})
 
-});
+router.post('/svm', function (req, res, next) {
+  // console.log("FormData "+ req.body.base64);
+  var emotion = req.body.emotion
+  var base64Data = req.body.base64.replace(/^data:image\/png;base64,/, '')
+  var timestamp = new Date().getTime()
+  var filename = emotion + '/web' + timestamp + '.png'
+  fs.writeFile('uploads/' + filename, base64Data, 'base64', function (err) {
+    if (err) {
+      console.log('error writing file: ' + err)
+    } else {
+      var options = {
+        mode: 'text',
+        pythonPath: '/usr/bin/python',
+        pythonOptions: ['-u'],
+        scriptPath: '/home/chverma/UPV/TFG/pythonDlibLendmark',
+        args: [filename]
+      }
+      PythonShell.run('predictOneImage.py', options, function (err, results) {
+        if (err) {
+          console.log('predictOneImage-> Error')
+          throw err
+        }
+        // results is an array consisting of messages collected during execution
+        console.log('results: %j', results.toString())
+        var emotionMsg = 'La teua emoció és: ' + results.toString()
+        console.log(emotionMsg)
+        res.send(JSON.stringify({'status': 1, 'msg': emotionMsg.toString()}))
+      })
+    }
+  })
 
-module.exports = router;
+  router.get('/revisor', function (req, res, next) {
+    // res.render('index', { title: 'Express'});
+    res.render('revisor', {title: 'Revisor'})
+  })
+})
+
+module.exports = router
